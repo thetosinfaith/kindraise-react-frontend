9ebcad0793274d1090c0f065bbb2b4c366d7645e
Oluwatosin Faith Ogungbaye <hellotosinfaith@gmail.com>
2024-08-24 18:10:34 +0100

    Create: Auth

---
+++ src/auth/CreateNewPassword/CreateNewPassword.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const CreateNewPassword = () => {
+  return (
+    <div>CreateNewPassword</div>
+  )
+}
+
+export default CreateNewPassword
---
+++ src/auth/VerifiedSuccessfully/VerifiedSuccessfully.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const VerifiedSuccessfully = () => {
+  return (
+    <div>VerifiedSuccessfully</div>
+  )
+}
+
+export default VerifiedSuccessfully
