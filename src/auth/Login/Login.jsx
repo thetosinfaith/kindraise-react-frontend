db8d89600e70508e20d83b8ce75b486c2ac9f4f0
Samuel <jacksamuel089@gmail.com>
2024-08-27 12:08:34 +0100

    i made some changes

--- src/auth/Login/Login.jsx
+++ src/auth/Login/Login.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import './login.css'
+import { useNavigate } from 'react-router-dom'
 
 const Login = () => {
+  const Nav = useNavigate()
   return (
-    <div>Login</div>
+    <div className='loginBody'>
+      <div className='loginBox'>
+        <div>Logo</div>
+        <input type="text" className='loginInput' placeholder='Email'/>
+        <input type="text" className='loginInput' placeholder='Password'/>
+        <div className='inputText'>
+          <div>Forgot password</div>
+          <div onClick={()=>Nav('/sighup')}>Sigh up</div>
+        </div>
+        <button className='loginBtn' onClick={()=>Nav('/dashboard')}>burron</button>
+      </div>
+    </div>
   )
 }
 
--- src/auth/Signup/Signup.jsx
+++ src/auth/Signup/Signup.jsx
@@ -1,9 +1,24 @@
 import React from 'react'
+import './sighup.css'
+import { useNavigate } from 'react-router-dom'
 
-const Signup = () => {
+const Sighup = () => {
+  const Nav= useNavigate()
   return (
-    <div>Signup</div>
+    <div className='sighupBody'>
+      <div className="sighupBox">helk</div>
+      <div className="sighupWrapper">
+        <div className='sighupInputArea'>
+          <input type="text" placeholder='Fullname'/>
+          <input type="text" placeholder='Email'/>
+          <input type="text" placeholder='Password'/>
+          <input type="text" placeholder='Email'/>
+          <div onClick={()=>Nav('/login')} style={{cursor: 'pointer'}}>login</div>
+          <button className='sighupBtn' onClick={()=>Nav('/login')}>click</button>
+        </div>
+      </div>
+    </div>
   )
 }
 
-export default Signup
+export default Sighup
