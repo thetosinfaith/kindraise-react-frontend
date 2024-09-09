d611d885ca98b50b22fe309f06be763c17348d39
Oluwatosin Faith Ogungbaye <hellotosinfaith@gmail.com>
2024-09-09 12:35:15 +0100

    updated the path

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import Signup from "./auth/Signup/Signup";
 import Login from "./auth/Login/Login";
 import PasswordChange from "./auth/PasswordChanged/PasswordChange";
 import CreateNewPassword from "./auth/CreateNewPassword/CreateNewPassword";
-import ResetPassword from "./auth/Resetpassword/ResetPassword";
+import ResetPassword from "./auth/ResetPassword/ResetPassword";
 import DashBoard from "./pages/Dashboard/Dashboard";
 import Campaign from "./pages/Campaign/Campaign";
 import Donor from "./pages/Donor/Donor";
